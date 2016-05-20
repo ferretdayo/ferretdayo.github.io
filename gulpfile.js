e2c06b3282117c5a2fbcede7a8fcf6bdd356f0cd
ferretdayo <ferretdayo@hotmail.co.jp>
2016-05-20 15:20:13 +0900

    gulp-watchをgulp.watchni

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require("gulp");
-var watch = require('gulp-watch');
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var browserSync = require("browser-sync");
@@ -10,12 +9,12 @@ gulp.task('default', ['sass', 'browser-sync', 'pug', 'watch']);
 
 //sassとpugの監視をして変換処理させる
 gulp.task('watch', () => {
-    watch(['./sass/**'], () => {
+    gulp.watch(['./sass/**'], () => {
         gulp.start(['sass']);
     });
-    watch(['./pug/**'], () => {
+    gulp.watch(['./pug/**'], () => {
         gulp.start(['pug']);
-    })
+    });
 });
 
 //ブラウザ表示
@@ -56,6 +55,6 @@ gulp.task("pug", () => {
 });
 
 //ブラウザリロード処理
-gulp.task('reload', function () {
+gulp.task('reload', () => {
     browserSync.reload();
 });
